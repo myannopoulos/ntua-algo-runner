695c134117f3dc1c913d3fae1acaf88f107ed8c7
myannopoulos <71171874+myannopoulos@users.noreply.github.com>
2020-11-10 20:11:13 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 1 ] ; then
+    echo 'Please give exactly one argument, your program name'
+    exit 0
+fi
+
+if [ -f "$1" ] ; then
+	for file in input*.txt
+	do
+		./$1 < $file > test.txt
+		num=$(echo $file | sed 's/[^0-9]*//g')
+		outputfile="output$num.txt"
+		DIFF=$(diff test.txt $outputfile)
+		if [ "$DIFF" ] ; then
+			echo "ERROR running $file"
+			echo $DIFF
+		fi
+	done
+else
+	echo "File not found"
+fi
